3bc1d5dae35cca82f5256be4c416a0b359eede06
Mark A. <mark001616@gmail.com>
2024-12-23 22:19:02 -0500

    Add initial commit

---
+++ src/components/Navbar.jsx
@@ -0,0 +1,8 @@
+export default function Navbar() {
+  return (
+    <div className="navbar-container">
+      <div className="navbar-home-btn">home</div>
+      <div className="navbar-credits-btn">credits</div>
+    </div>
+  );
+}
