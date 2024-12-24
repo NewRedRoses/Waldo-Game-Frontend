73a4743c60d5dc3c175d44d53657365b198bbdc6
Mark A. <mark001616@gmail.com>
2024-12-24 14:50:31 -0500

    Add pokemon image and folder

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,24 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import "./App.css";
+import PokemonWaldoPicture from "./Images/LFcompletepg.jpg";
 
 function App() {
   const [count, setCount] = useState(0);
 
+  function mouseClick(event) {
+    console.log(event.clientX, event.clientY);
+  }
   return (
     <>
-      <p>test</p>
+      <p>
+        Select 3 Pokemon: <i>Squirtle</i>, <i>Sandshrew</i> and <i>Starmie</i>
+      </p>
+      <img
+        src={PokemonWaldoPicture}
+        alt="Pallet Town filled with a wide range of Pokemons cluttered everywhere"
+        width={800}
+        onClick={(e) => mouseClick(e)}
+      />
     </>
   );
 }
