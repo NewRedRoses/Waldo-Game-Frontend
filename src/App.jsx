c1429180e17b79b1917b7700d10b0f25a468c3e9
Mark A. <mark001616@gmail.com>
2024-12-29 16:33:37 -0500

    Make pokemon list dynamic and send post request

--- src/App.jsx
+++ src/App.jsx
@@ -2,34 +2,21 @@ import { useEffect, useState } from "react";
 import "./App.css";
 import PokemonWaldoPicture from "./Images/LFcompletepg.jpg";
 import DropdownMenu from "./components/DropdownMenu";
+import axios from "axios";
+
 function App() {
+  const [pokemons, setPokemons] = useState([]);
   const [clickCoords, setClickCoords] = useState({ x: 0, y: 0 });
   const [pokemonSelected, setPokemonSelected] = useState("");
   const [showDropdown, setShowDropdown] = useState(false);
 
   const backendUrl = "http://localhost:3000/";
-  // a quite bit of the code below is merely 'logic'. Therefore, it will need to be
-  // moved to backend
-  //
-  const sampleList = ["charizard", "squirtle", "charmander"];
 
-  const pokemons = [
-    {
-      name: "Psyduck",
-      coordinates: {
-        topLeftX: 391,
-        topLeftY: 517,
-        topRightX: 426,
-        topRightY: 520,
-        bottomLeftX: 393,
-        bottomLeftY: 551,
-        bottomRightX: 429,
-        bottomRightY: 554,
-      },
-    },
-  ];
-
-  useEffect(() => {}, [clickCoords]);
+  useEffect(() => {
+    axios(backendUrl + "pokemons").then((response) =>
+      setPokemons(response.data),
+    );
+  }, []);
 
   function handleMouseClick(e) {
     setClickCoords({ x: e.clientX, y: e.clientY });
@@ -39,28 +26,25 @@ function App() {
   return (
     <div className="content">
       <div className="greeting-msg-container">
-        <p>Select 3 Pokemon:</p>
-        <ul>
-          <li>Squirtle</li>
-          <li>Sandshrew</li>
-          <li>Starmie</li>
+        <p>Select these 3 Pokemon in the picture:</p>
+        <ul className="pokemons-container">
+          {pokemons.map((pk) => (
+            <li key={pk.id} className="pokemon-item">
+              <img src={pk.sprite} alt={`${pk.name}'s picture`} width="100" />
+              {pk.name}
+            </li>
+          ))}
         </ul>
       </div>
-      {console.log(clickCoords, pokemonSelected)}
       <img
         src={PokemonWaldoPicture}
         alt="Pallet Town filled with a wide range of Pokemons cluttered everywhere"
         width={800}
         onClick={(e) => handleMouseClick(e)}
       />
-      <form action={backendUrl} method="POST">
-        <button type="submit" onClick={(e) => console.log("btn pressed")}>
-          save
-        </button>
-      </form>
       {showDropdown && (
         <DropdownMenu
-          list={sampleList}
+          list={pokemons}
           caption={"Which Pokemon is this?"}
           urlToSendTo={backendUrl + "validate"}
           position={clickCoords}
